fac8d5826206b885d1383d7541ce03432e8e4903
Daniel <78626236+too-many-hats@users.noreply.github.com>
2023-12-04 08:28:15 +1100

    Add orignal photos and documentation

--- BinUtils/Character.cs
+++ BinUtils/Character.cs
@@ -1,7 +1,8 @@
 ﻿namespace BinUtils;
 
-public record Character(int Value, char Char)
+public record Character(int Value, char Char, bool CheckBit)
 {
     public int Value { get; set; } = Value;
     public char Char { get; set; } = Char;
+    public bool CheckBit { get; init; } = CheckBit;
 }
